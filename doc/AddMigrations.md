2801e606e474379074908eb40744f710bcdb9c2d
JoaoLucasXavier <lucasxavier000@gmail.com>
2020-10-22 18:15:28 -0300

    alterar projeto web de Apresentação para Web

--- doc/AddMigrations.md
+++ doc/AddMigrations.md
@@ -1,4 +1,4 @@
 - Migration path: eccommerce-system\src
 
-- CLI: dotnet ef migrations add Initial -s Presentation -p Infra --context ECommerceContext -v
-- CLI: dotnet ef database update -s Presentation -p Infra --context ECommerceContext -v
+- CLI: dotnet ef migrations add Initial -s Web -p Infra --context ECommerceContext -v
+- CLI: dotnet ef database update -s Web -p Infra --context ECommerceContext -v
---
+++ src/Web/Controllers/ProductsController.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Interfaces;
+using Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    public class ProductsController : Controller
+    {
+        public readonly ProductAppInterface _productAppInterface;
+        public readonly UserManager<ApplicationUser> _userManager;
+
+        public ProductsController(ProductAppInterface productAppInterface, UserManager<ApplicationUser> userManager)
+        {
+            _productAppInterface = productAppInterface;
+            _userManager = userManager;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _productAppInterface.ListUserProducts(await getLoggedUserId()));
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(Guid id)
+        {
+            return View(await _productAppInterface.GetEntityById(id));
+        }
+
+        // GET: Products/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product)
+        {
+            try
+            {
+                product.UserId = await getLoggedUserId();
+                await _productAppInterface.AddProduct(product);
+                if (product.Notifications.Any())
+                {
+                    foreach (var item in product.Notifications)
+                    {
+                        ModelState.AddModelError(item.PropertyName, item.Message);
+                    }
+                    return View("Create", product);
+                }
+            }
+            catch
+            {
+                return View("Create", product);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Products/Edit/5
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            return View(await _productAppInterface.GetEntityById(id));
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Product product)
+        {
+            try
+            {
+                await _productAppInterface.UpdateProduct(product);
+                if (product.Notifications.Any())
+                {
+                    foreach (var item in product.Notifications)
+                    {
+                        ModelState.AddModelError(item.PropertyName, item.Message);
+                    }
+                    return View("Edit", product);
+                }
+            }
+            catch
+            {
+                return View("Edit", product);
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Products/Delete/5
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return View(await _productAppInterface.GetEntityById(id));
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Guid id, Product product)
+        {
+            try
+            {
+                var productToDelete = await _productAppInterface.GetEntityById(id);
+                await _productAppInterface.Delete(productToDelete);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private async Task<string> getLoggedUserId()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            return user.Id;
+        }
+    }
+}
